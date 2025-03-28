db18c1e63ff98b9d6ccbaa7f87fdafcdb39fab92
Elango-Sagravia <elango@sagravia.com>
2025-03-28 17:02:11 +0530

    fixed big screen issue

--- app/(web)/archives/page.js
+++ app/(web)/archives/page.js
@@ -30,9 +30,9 @@ async function getData() {
 export default async function archive() {
   const blogs = await getData();
   return (
-    <main className="">
+    <main className="bg-nl_sec_background min-h-screen">
       <ArchiveHeader />
-      <section className="px-4 md:px-16 pt-4 pb-32 max-w-7xl mx-auto bg-nl_sec_background">
+      <section className="px-4 md:px-16 pt-4 pb-32 max-w-7xl mx-auto ">
         <GridContainer hideButton={true} articles={blogs} />
       </section>
       <Subscribe />
