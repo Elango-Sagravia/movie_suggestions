4328a01861d41d895077b8181022862b875c5c0e
Elango-Sagravia <elango@sagravia.com>
2025-03-31 13:16:44 +0530

    updated active subscribers filter

--- components/ui/complainceHeader/complainceHeader.js
+++ components/ui/complainceHeader/complainceHeader.js
@@ -3,7 +3,7 @@ function ComplainceHeader({ title, date }) {
     <div className="bg-nl_sec_background">
       <div className="w-full md:w-4/5 lg:w-2/3 px-4 md:px-0 mx-auto pb-16 lg:pt-44 pt-40 max-w-7xl">
         <h1 className="text-4xl text-nl_background">{title}</h1>
-        <p className="text-sm mt-4">Updated: {date}</p>
+        <p className="text-sm mt-4 text-nl_background">Updated: {date}</p>
       </div>
     </div>
   );
